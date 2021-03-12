a456fdc12db4fca51752712c44a9507d5e946009
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2021-03-12 18:32:28 +0000

    Add program 1.1

---
+++ 1.1 - Display Your Name/main.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Name_Label = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Name_Label
+        '
+        Me.Name_Label.AutoSize = True
+        Me.Name_Label.BackColor = System.Drawing.Color.Transparent
+        Me.Name_Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 128.0!)
+        Me.Name_Label.Location = New System.Drawing.Point(33, 100)
+        Me.Name_Label.Name = "Name_Label"
+        Me.Name_Label.Size = New System.Drawing.Size(709, 243)
+        Me.Name_Label.TabIndex = 0
+        Me.Name_Label.Text = "Kalam"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Name_Label)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Name_Label As Label
+End Class
