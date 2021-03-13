ad626837d58258ce65bbf937473f638086ea4f24
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2021-03-13 08:41:24 +0000

    Create program 3.1

---
+++ 3.1 - Add two numbers/main.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNotice = New System.Windows.Forms.Label()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblNotice
+        '
+        Me.lblNotice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNotice.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblNotice.Location = New System.Drawing.Point(12, 45)
+        Me.lblNotice.Name = "lblNotice"
+        Me.lblNotice.Size = New System.Drawing.Size(297, 30)
+        Me.lblNotice.TabIndex = 0
+        Me.lblNotice.Text = "The sum of the two numbers is"
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblOutput.Location = New System.Drawing.Point(315, 45)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(98, 30)
+        Me.lblOutput.TabIndex = 1
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(425, 158)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.lblNotice)
+        Me.Name = "main"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lblNotice As Label
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ 3.1 - Add two numbers/main.vb
@@ -0,0 +1,12 @@
+﻿Public Class main
+    Dim first_number As Integer
+    Dim second_number As Integer
+    Dim total As Integer
+
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        first_number = InputBox("Enter your first number")
+        second_number = InputBox("Enter your second number")
+        total = first_number + second_number
+        lblOutput.Text = total
+    End Sub
+End Class
