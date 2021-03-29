46bea01385a00eabf8d3480bc5c030cb3da25843
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2021-03-29 12:31:26 +0100

    Create program 6.1

---
+++ 6.1 - Deciding exam grades/MAIN.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Mark As Integer
+        Mark = InputBox("Enter an exam mark from 0 to 100")
+        If Mark >= 60 Then
+            MsgBox("Merit")
+        ElseIf Mark Then >= 40 Then
+            MsgBox("Pass")
+        Else
+            MsgBox("A mark of " & Mark & "is a fail")
+        End If
+    End Sub
+End Class
