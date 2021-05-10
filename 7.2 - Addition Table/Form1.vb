1ffc46fadf4c9917c65cd79f0d50f6154687921b
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2021-05-10 11:43:16 +0100

    Create Program 7.2

---
+++ 7.2 - Addition Table/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Const Max = 5
+        Dim ColNumber, RowNumber, Sum As Integer
+        Dim Message As String
+
+        Message = "+" & Space(11)
+        For ColNumber = 0 To Max
+            Message = Message & ColNumber & Space(8)
+        Next ColNumber
+        Message = Message & vbNewLine & vbNewLine
+        For RowNumber = 0 To Max
+            Message = Message & RowNumber & Space(12)
+            For ColNumber = 0 To Max
+                Sum = ColNumber + RowNumber
+                Message = Message & Sum & Space(8)
+            Next ColNumber
+            Message = Message & vbNewLine
+        Next RowNumber
+        MsgBox(Message, , "Addition table using a nested For Loop")
+    End Sub
+End Class
---
+++ 7.2 - Addition Table/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("7.2 - Addition Table")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("7.2 - Addition Table")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9332322-1611-48bd-8361-ebe000a8db03")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
