9d64d3bc9b2e0fb52966860cc946421a1e966781
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2021-05-10 09:29:37 +0100

    Create EofExercise 6 1

---
+++ 6.5 - End of Chapter Ex 1/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim TotalSales As Integer
+        Dim Bonus As Integer
+
+        TotalSales = txtTotal.Text
+
+        If (TotalSales >= 100000) Then
+            Bonus = 10000
+        ElseIf (TotalSales >= 70000) Then
+            Bonus = 7000
+        ElseIf (TotalSales >= 50000) Then
+            Bonus = 4000
+        Else
+            Bonus = 0
+        End If
+
+
+        MsgBox("The bonus you earned this year : £" + CStr(Bonus))
+
+    End Sub
+End Class
