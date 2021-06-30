604ede85576fda660f6e4e8d43b4416cb49ef959
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2021-06-30 21:38:32 +0100

    Finish EofC 10.5

---
+++ 10.4 - End of Exercise 5/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lst_City = New System.Windows.Forms.ListBox()
+        Me.lst_Countries = New System.Windows.Forms.ListBox()
+        Me.lblCheck = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lst_City
+        '
+        Me.lst_City.FormattingEnabled = True
+        Me.lst_City.ItemHeight = 16
+        Me.lst_City.Items.AddRange(New Object() {"London", "Paris", "Tokyo", "Beijing", "Manila", "Washington D.C"})
+        Me.lst_City.Location = New System.Drawing.Point(35, 43)
+        Me.lst_City.Name = "lst_City"
+        Me.lst_City.Size = New System.Drawing.Size(120, 116)
+        Me.lst_City.TabIndex = 0
+        '
+        'lst_Countries
+        '
+        Me.lst_Countries.FormattingEnabled = True
+        Me.lst_Countries.ItemHeight = 16
+        Me.lst_Countries.Items.AddRange(New Object() {"Phillipines", "China", "USA", "Japan", "England", "France"})
+        Me.lst_Countries.Location = New System.Drawing.Point(198, 43)
+        Me.lst_Countries.Name = "lst_Countries"
+        Me.lst_Countries.Size = New System.Drawing.Size(120, 116)
+        Me.lst_Countries.TabIndex = 1
+        '
+        'lblCheck
+        '
+        Me.lblCheck.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCheck.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblCheck.Location = New System.Drawing.Point(376, 27)
+        Me.lblCheck.Name = "lblCheck"
+        Me.lblCheck.Size = New System.Drawing.Size(173, 51)
+        Me.lblCheck.TabIndex = 2
+        Me.lblCheck.Text = "Enter Guess"
+        Me.lblCheck.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.lblAnswer.Location = New System.Drawing.Point(341, 108)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(255, 148)
+        Me.lblAnswer.TabIndex = 3
+        Me.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(609, 290)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.lblCheck)
+        Me.Controls.Add(Me.lst_Countries)
+        Me.Controls.Add(Me.lst_City)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lst_City As ListBox
+    Friend WithEvents lst_Countries As ListBox
+    Friend WithEvents lblCheck As Label
+    Friend WithEvents lblAnswer As Label
+End Class
---
+++ 10.4 - End of Exercise 5/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Function CheckAnswer(ByVal CityName As String, ByVal CountryName As String)
+        Dim City_Names(6) As String
+        Dim Country_Names(6) As String
+        City_Names(0) = "London"
+        City_Names(1) = "Paris"
+        City_Names(2) = "Tokyo"
+        City_Names(3) = "Beijing"
+        City_Names(4) = "Manila"
+        City_Names(5) = "Washington D.C"
+        Country_Names(0) = "England"
+        Country_Names(1) = "France"
+        Country_Names(2) = "Japan"
+        Country_Names(3) = "China"
+        Country_Names(4) = "Phillipines"
+        Country_Names(5) = "USA"
+        Dim cityindex As Integer = Array.IndexOf(City_Names, CityName)
+        If Country_Names(cityindex) = CountryName Then
+            lblCheck.Text = "CORRECT"
+            System.Threading.Thread.Sleep(1000)
+            lst_City.SelectedIndex = -1
+            lst_Countries.SelectedIndex = -1
+            Return True
+        Else
+            lblCheck.Text = Country_Names(cityindex)
+            lblAnswer.Text = CountryName & "'s capital city is " & Country_Names(cityindex)
+            lst_City.SelectedIndex = -1
+            lst_Countries.SelectedIndex = -1
+            Return False
+        End If
+    End Function
+
+    Private Sub lst_City_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lst_City.SelectedIndexChanged
+        If lst_Countries.SelectedIndex >= 0 Then
+            lblCheck.Text = lst_City.SelectedItem
+            CheckAnswer(lst_City.SelectedItem, lst_Countries.SelectedItem)
+
+        End If
+    End Sub
+
+    Private Sub lst_Countries_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lst_Countries.SelectedIndexChanged
+        If lst_City.SelectedIndex >= 0 Then
+            CheckAnswer(lst_City.SelectedItem, lst_Countries.SelectedItem)
+        End If
+    End Sub
+End Class
